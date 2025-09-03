c9ccc5ecea14a83b00a87e61eaa6e964da14737d
Leandro Bento <55304620+leandrobento84@users.noreply.github.com>
2025-09-02 23:24:34 -0300

    02092025_23h24
    Validando classe carro

---
+++ Carro.java
@@ -0,0 +1,8 @@
+package LojaNarutoUzumaki;
+
+public class Carro {
+    public static void main(String[] args){
+        System.out.print("Qual é seu carro: ");
+        System.out.println("Tuscon Motor V6 | 2.7cc");
+    }
+}
